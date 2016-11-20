e1fbe9d378c2b6a314cb3539d9fe0ba8924fed7c
Maarten de Jager <maartendejager@gmail.com>
2016-11-20 17:12:28 +0100

    Adds heading to the page and lorem for a bit of intro text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.master')
 
 @section('content')
+	<div class="row">
+	    <div class="col-md-4" >
+	        <h1 class="parel_ttl">Parels in de regio</h1>
+	    </div>
+
+	    <div class="col-md-8">
+	    	<div class="contentelement">
+	    	    <h2>Parels van Voion</h2>     
+	    	    <div>
+	    	    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+	    	    	<a href="#">Bekijk hier het groeidocument</a>
+	    	    </div>
+	    </div>
+    </div>
+
     <div class="row">
         <div class="col-md-12" id="pearls">
-            <h1>Parels van Voion</h1>
             <pearls></pearls>
         </div>
     </div>
