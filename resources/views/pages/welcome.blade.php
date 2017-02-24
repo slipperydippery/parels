87c4166d994c3d16d3cf7fb28a70a9acdda9297c
Maarten de Jager <maartendejager@gmail.com>
2017-02-24 15:29:16 +0100

    moves text up

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,6 +1,15 @@
         <div class="pearls">
             <div class="row entree" >
                 <h1 class="programmalijn">Parels uit de regio</h1>   
+                <div class="introtext">
+                <p>Voion presenteert 24 parels uit de regio. Prachtige verhalen van scholen in het voortgezet onderwijs die u inspireren en helpen bij:
+                <ul>
+                    <li>opleiding & professionalisering</li>
+                    <li>arbeidsmarkt en mobiliteit</li>
+                    <li>veilig en gezond werken</li>
+                </ul>
+                    Wij presenteren de parels dit jaar een voor een. Klik op de animatiefilm voor een overzicht van de parels of op een parel hieronder.</p>
+                </div>
                 <div class="overview--video">
                     <div class="flex-video widescreen inblockvideo">
                         <img
@@ -12,15 +21,7 @@
                 </div>
 
                 <div class="">
-                        <div class="introtext">
-                            <p>Voion presenteert 24 parels uit de regio. Prachtige verhalen van scholen in het voortgezet onderwijs die u inspireren en helpen bij:
-<ul>
-    <li>opleiding & professionalisering</li>
-    <li>arbeidsmarkt en mobiliteit</li>
-    <li>veilig en gezond werken</li>
-</ul>
-    Wij presenteren de parels dit jaar een voor een. Klik op de animatiefilm voor een overzicht van de parels of op een parel hieronder.</p>
-                        </div>
+
                 </div>
 
 
